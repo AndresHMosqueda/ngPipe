7a354a2558fc7fd318abf5a53d8be89aef64aba4
AndresHMosqueda <andreshm94@gmail.com>
2019-06-25 11:43:09 -0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Andres';
+  nombre2 = "andres hernandez"
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+
+  valorDePromesa = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve('Llego la info'), 3000)
+  })
+
+
+  fecha = new Date();
+
+  activar:boolean = true;
 }
